465a5f24cfe6909a95440b66f0c04a08e6932ca6
John Chabla <jean-baptiste.chabi@takkatech.com>
2024-08-23 15:36:28 +0100

    correction

--- resources/js/components/navbar.vue
+++ resources/js/components/navbar.vue
@@ -92,16 +92,16 @@
               href="tel:22969251018"
               >Passer un appel</a
             >
-            <!--div class="dHctbWwtNA md:dHctaGlkZGVu">
+            <div class="dHctbWwtNA md:dHctaGlkZGVu">
               <div data-headlessui-state="">
                 <button
                   class="dHctZ3JvdXA dHctcmVsYXRpdmU dHctei01MA dHctZmxleA dHctY3Vyc29yLXBvaW50ZXI dHctaXRlbXMtY2VudGVy dHctanVzdGlmeS1jZW50ZXI dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtMTAwLzgw dHctcC0z dHctc2hhZG93LXNt dHctc2hhZG93LXNreS0xMDAvNTA dHctcmluZy0x dHctcmluZy1zbGF0ZS05MDAvNQ dHctdHJhbnNpdGlvbg dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ hover:dHctYmctc2xhdGUtMjAwLzYw focus:dHctb3V0bGluZS1ub25l md:dHctaGlkZGVu"
                   aria-label="Toggle Navigation"
                   type="button"
                   aria-expanded="false"
-                  data-headlessui-state=""
+                  data-headlessui-state="" @click="changeMenu"
                 >
-                  <span class="dHctcmVsYXRpdmU dHctaC0z.5 dHctdy00">
+                  <span class="dHctcmVsYXRpdmU dHctaC0z.5 dHctdy00" v-if="!defaultMenu">
                     <span
                       class="dHctYWJzb2x1dGU dHctYmxvY2s dHctaC0w.5 dHctcm90YXRlLTA dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw dHctbGVmdC0w dHctdG9wLTA dHctdy1mdWxs"
                     ></span
@@ -115,7 +115,87 @@
                       class="dHctYWJzb2x1dGU dHctYmxvY2s dHctaC0w.5 dHctcm90YXRlLTA dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw dHctbGVmdC0w dHctdG9wLTM dHctdy1mdWxs"
                     ></span
                   ></span>
+
+                  <span class="dHctcmVsYXRpdmU dHctaC0z.5 dHctdy00" v-if="defaultMenu">
+                    <span class="dHctYWJzb2x1dGU dHctYmxvY2s dHctaC0w.5 dHctcm90YXRlLTA dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw dHctbGVmdC0x/2 dHctdG9wLTE.5 dHctdy0w"></span>
+                    <span class="dHctYWJzb2x1dGU dHctbGVmdC0w dHctdG9wLTE.5 dHctYmxvY2s dHctaC0w.5 dHctdy1mdWxs dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw dHctcm90YXRlLTQ1"></span>
+                    <span class="dHctYWJzb2x1dGU dHctbGVmdC0w dHctdG9wLTE.5 dHctYmxvY2s dHctaC0w.5 dHctdy1mdWxs dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw LXR3LXJvdGF0ZS00NQ"></span>
+                    <span class="dHctYWJzb2x1dGU dHctYmxvY2s dHctaC0w.5 dHctcm90YXRlLTA dHctdHJhbnNmb3Jt dHctcm91bmRlZC1mdWxs dHctYmctc2xhdGUtNzAw dHctb3BhY2l0eS0xMDA dHctdHJhbnNpdGlvbi1hbGw dHctZHVyYXRpb24tMzAw dHctZWFzZS1pbi1vdXQ group-hover:dHctYmctc2xhdGUtOTAw dHctbGVmdC0x/2 dHctdG9wLTE.5 dHctdy0w"></span>
+                  </span>
                 </button>
+
+                <!--Menu sur le mobile-->
+                <div v-if="defaultMenu">
+
+                  <div
+                    class="dHctZml4ZWQ dHctaW5zZXQtMA dHctei0yMA dHctYmctc2xhdGUtOTAw dHctYmctb3BhY2l0eS01MA dHctb3BhY2l0eS0xMDA"
+                    id="headlessui-popover-overlay-:ro:"
+                    aria-hidden="true"
+                    data-headlessui-state="open" @click="defaultMenu = false">
+                  </div>
+
+                  <div
+                    class="dHctYWJzb2x1dGU dHctaW5zZXQteC0w dHctdG9wLWZ1bGw dHctei0zMA dHctbXQtNA dHctb3JpZ2luLXRvcA dHctb3ZlcmZsb3ctaGlkZGVu dHctcm91bmRlZC0yeGw dHctYmctc2xhdGUtNTA dHctcHgtNg dHctcHktNw dHctc2hhZG93LXhs dHctc2hhZG93LXNreS0xMDAvNDA dHctcmluZy0x dHctcmluZy1zbGF0ZS05MDAvNQ dHctb3BhY2l0eS0xMDA dHctc2NhbGUtMTAw"
+                    id="headlessui-popover-panel-:rp:"
+                    tabindex="-1"
+                    data-headlessui-state="open">
+                    
+                    <div>
+                      <div class="dHctZmxleA dHctZmxleC1jb2w dHctc3BhY2UteS00">
+                        <router-link
+                          class="dHctYmxvY2s dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctdGV4dC1zbGF0ZS03MDA dHctZHVyYXRpb24tMjAw hover:dHctdGV4dC1zbGF0ZS05MDA "
+                          to="/" @click="defaultMenu = false" :style="($route.path == '/') ? { color: 'black', fontWeight: 900 } : {}">
+                          Accueil
+                        </router-link>
+                        <router-link
+                          class="dHctYmxvY2s dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctdGV4dC1zbGF0ZS03MDA dHctZHVyYXRpb24tMjAw hover:dHctdGV4dC1zbGF0ZS05MDA"
+                          to="/about" @click="defaultMenu = false" :style="($route.path == '/about') ? { color: 'black', fontWeight: 900 } : {}">
+                          À Propos
+                        </router-link>
+                        <router-link
+                          class="dHctYmxvY2s dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctdGV4dC1zbGF0ZS03MDA dHctZHVyYXRpb24tMjAw hover:dHctdGV4dC1zbGF0ZS05MDA"
+                          to="/work" @click="defaultMenu = false" :style="($route.path == '/work') ? { color: 'black', fontWeight: 900 } : {}">
+                          Réalisations
+                        </router-link>
+                        <router-link
+                          class="dHctYmxvY2s dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctdGV4dC1zbGF0ZS03MDA dHctZHVyYXRpb24tMjAw hover:dHctdGV4dC1zbGF0ZS05MDA"
+                          to="/blog" @click="defaultMenu = false" :style="($route.path == '/blog') ? { color: 'black', fontWeight: 900 } : {}">
+                          Blog
+                        </router-link>
+                        <router-link
+                          class="dHctYmxvY2s dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctdGV4dC1zbGF0ZS03MDA dHctZHVyYXRpb24tMjAw hover:dHctdGV4dC1zbGF0ZS05MDA"
+                          to="/contact" @click="defaultMenu = false" :style="($route.path == '/contact') ? { color: 'black', fontWeight: 900 } : {}">
+                          Contacts
+                        </router-link >
+
+                        <!--div class="dHctcmVsYXRpdmU" data-headlessui-state="">
+                          <button
+                            class="dHctZ3JvdXA dHctZmxleA dHctdy1mdWxs dHctaXRlbXMtY2VudGVy dHctZ2FwLTI dHctdGV4dC1iYXNl dHctZm9udC1zZW1pYm9sZA dHctZHVyYXRpb24tMjAw dHctZWFzZS1pbi1vdXQ dHctdGV4dC1zbGF0ZS03MDA hover:dHctdGV4dC1zbGF0ZS05MDA"
+                            id="headlessui-disclosure-button-:rs:"
+                            type="button"
+                            aria-expanded="false"
+                            data-headlessui-state="">
+                            <span>Pages</span>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 20 20"
+                              fill="currentColor"
+                              class="dHctaC01 dHctdy01 dHctZHVyYXRpb24tMzAw dHctdGV4dC1zbGF0ZS03MDA group-hover:dHctdGV4dC1zbGF0ZS05MDA"
+                              aria-hidden="true">
+                              <path
+                                fill-rule="evenodd"
+                                d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
+                                clip-rule="evenodd">
+                              </path>
+                            </svg>
+                          </button>
+                        </div-->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <!--Menu sur le mobile-->
+
               </div>
               <div
                 style="
@@ -133,7 +213,7 @@
                   display: none;
                 "
               ></div>
-            </div-->
+            </div>
           </div>
         </nav>
       </div>
@@ -145,9 +225,14 @@
 export default{
     data(){
         return {
-
+          defaultMenu: false,
         }
     },
+    methods: {
+      changeMenu(){
+        this.defaultMenu = !this.defaultMenu;
+      }
+    }
    
 }
 </script>
